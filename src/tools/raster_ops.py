5c115a5d81297aac0db142fe6e3ae21076ddbfa1
Patryk Młynarski <patryk.mlynarski98@gmail.com>
2026-03-24 21:27:30 +0100

    feat: started to fill methods within raster ops

--- src/tools/raster_ops.py
+++ src/tools/raster_ops.py
@@ -1,23 +1,56 @@
-from osgeo import gdal
-from pathlib import Path
+# TODO to będą operacje na całym rastrze, potrzebne tylko ścieżki
+import rasterio
+import numpy as np
+from typing import Any
 
-CURRENT_FILE = Path(__file__).resolve()
-PROJECT_ROOT = CURRENT_FILE.parents[2]
-
-dataset = gdal.Open(
-    "D:\\repos\\sentinel_data_downloader\\data\\S2A_MSIL2A_20230928T093031_N0509_R136_T34UEB_20230928T135957.tif",
-    gdal.GA_ReadOnly,
-)
-band = dataset.GetRasterBand(1)
-print("Band type ={}".format(gdal.GetDataTypeName(band.DataType)))
+RED_BAND = 1
+GREEN_BAND = 2
+NIR_BAND = 4
 
 
-scanline = band.ReadRaster(
-    xoff=0,
-    yoff=0,
-    xsize=band.XSize,
-    ysize=1,
-    buf_xsize=band.XSize,
-    buf_ysize=1,
-    buf_type=gdal.GDT_Float32,
-)
+def read_raster_bands(file_path: str) -> tuple[Any, Any, Any]:
+    with rasterio.open(file_path) as band:
+        green_band = band.read(GREEN_BAND).astype("float32")
+        red_band = band.read(RED_BAND).astype("float32")
+        nir_band = band.read(NIR_BAND).astype("float32")
+
+    return green_band, red_band, nir_band
+
+
+def denominate_bands(*args):
+    return args[0] + args[1]
+
+
+def write_raster():
+    pass
+
+
+def reproject_raster():
+    pass
+
+
+def generate_mask(*args: Any, **kwargs: Any) -> Any:
+    mask = (args[0] == 0) | (args[1] == 0)
+
+    return mask
+
+
+# TODO te 3 mozna wrzucic do innej klasy bo korzystaja z bandsów
+def generate_cir():
+    pass
+
+
+def calculate_ndvi(red_band: np.ndarray, nir_band: np.ndarray) -> np.ndarray:
+    pass
+
+
+def calculate_ndwi(green_band: np.ndarray, nir_band: np.ndarray) -> np.ndarray:
+    ndwi = np.full(green_band.shape, np.nan)
+    denom = denominate_bands(green_band, nir_band)
+    return np.divide(green_band - nir_band, denom, out=ndwi, where=denom != 0)
+
+    pass
+
+
+def generate_stats():
+    pass
